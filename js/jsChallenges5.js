200a33635e27efe331b52c97ea165182d56d6e53
little-ningmeng <62879775+little-ningmeng@users.noreply.github.com>
2024-04-04 20:54:18 -0400

    Delete js/jsChallenges5.js

--- js/jsChallenges5.js
+++
@@ -1,5 +0,0 @@
-document.querySelector("#bandana").addEventListener("mouseover", function() {
-    let pos = document.querySelector("#display");
-    pos.insertAdjacentHtml('beforeend', this.alt);
-    
-})
